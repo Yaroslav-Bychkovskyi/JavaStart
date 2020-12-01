ca2c775e0a69562d3ad10a36eea2f404ab71ff2f
Yaroslav Bychkovskyi <yaroslav1208@gmail.com>
2020-12-01 19:06:50 +0200

    Version 5.2

--- src/com/github/yb/section8/bookpack/Book1.java
+++ src/com/github/yb/section8/bookpack/Book1.java
@@ -1,53 +1,24 @@
 package com.github.yb.section8.bookpack;
 
+import com.github.yb.section8.bookpack.bok.ExtBook;
+
 public class Book1 {
     protected String title;
     protected String author;
     protected int pubDate;
 
-    Book1(String t, String a, int d) {
+    public Book1(String t, String a, int d) {
         title = t;
         author = a;
         pubDate = d;
     }
 
-    void show() {
+    public void show() {
         System.out.println(title);
         System.out.println(author);
         System.out.println(pubDate);
         System.out.println();
     }
-}
-
-class ExtBook extends Book1 {
-    private String publisher;
-
-    ExtBook(String t, String a, int d, String p) {
-        super(t, a, d);
-        publisher = p;
-    }
-
-    public void show() {
-        super.show();
-        System.out.println(publisher);
-        System.out.println();
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String p) {
-        publisher = p;
-    }
-
-    public String getTitle(){return title;}
-    public void setTitle(String t){title = t;}
-    public String getAuthor(){return author;}
-    public void setAuthor(String a){author = a;}
-    public int getPubDate(){return pubDate;}
-    public void setPubDate(int d){pubDate = d;}
-
 }
 
 class ProtectDemo{
