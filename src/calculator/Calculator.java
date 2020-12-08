89065293f231adbd6814b73cf53322722a666533
Yaroslav Bychkovskyi <yaroslav1208@gmail.com>
2020-12-08 16:14:51 +0200

    додає 2 числа без умов

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -10,8 +10,13 @@ public class Calculator {
         return num;
     }
 
+    String action() {
+        String symbol = in.next();
+        return symbol;
+    }
+
     public static void main(String[] args) {
-        int x, y;
+        int x, y, z;
         Calculator calculator = new Calculator();
         System.out.println("Введіть число 1: ");
         x = calculator.scaner();
@@ -19,6 +24,17 @@ public class Calculator {
         System.out.println("Введіть число 2: ");
         y = calculator.scaner();
 
+        System.out.println("Введіть дію: ");
+
+        String string;
+        string = calculator.action();
+        if (string.equals("+")) {
+
+            z = x + y;
+        } else z = 0;
+
+        System.out.println("Відповідь: ");
+        System.out.println(z);
 
     }
 }
