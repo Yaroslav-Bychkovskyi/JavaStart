a5ecbc17f858b0d21ed07d40ab6ded0b5d891b9e
Yaroslav Bychkovskyi <yaroslav1208@gmail.com>
2020-12-11 13:45:07 +0200

    Сортування масиву точок з компаратором

--- src/Array/Point3D.java
+++ src/Array/Point3D.java
@@ -1,6 +1,6 @@
 package Array;
 
-public class Point3D {
+public class Point3D implements Comparable<Point3D> {
     public int x;
     public int y;
     public int z;
@@ -24,5 +24,29 @@ public class Point3D {
         Point3D d = (Point3D) obj;
         boolean result = this.x == d.x && this.y == d.y && this.z == d.z;
         return (result);
+    }
+
+    @Override
+    public int compareTo(Point3D o) {
+        if (this.x > o.x) {
+            return 1;
+        }
+        if (this.x < o.x) {
+            return -1;
+        }
+        if (this.y > o.y) {
+            return 1;
+        }
+        if (this.y < o.y) {
+            return -1;
+        }
+        if (this.z > o.z) {
+            return 1;
+        }
+        if (this.z < o.z) {
+            return -1;
+        }
+
+        return 0;
     }
 }
--- src/Array/PointMain3D.java
+++ src/Array/PointMain3D.java
@@ -1,7 +1,11 @@
 package Array;
 
 
+import com.github.yb.section1_5.Bubble;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PointMain3D {
@@ -9,23 +13,19 @@ public class PointMain3D {
 
         List<Point3D> res = new ArrayList<>();
 
-        Point3D[] a = {new Point3D(10, 20, 30), new Point3D(42, 99, 45), new Point3D(10, 3,3),
-                new Point3D(97, 39, 19), new Point3D(17, 44, 67), new Point3D(48, 48,23)};
+        Point3D[] a = {new Point3D(10, 20, 30), new Point3D(42, 99, 45), new Point3D(10, 3, 3),
+                new Point3D(97, 39, 19), new Point3D(17, 44, 67), new Point3D(48, 48, 23)};
 
         Point3D[] b = {new Point3D(10, 3, 3), new Point3D(4, 9, 2), new Point3D(62, 15, 7),
-                new Point3D(12, 159, 234), new Point3D(97, 39,19), new Point3D(10, 20,30)};
+                new Point3D(12, 159, 234), new Point3D(97, 39, 19), new Point3D(10, 20, 30)};
 
+        System.out.println(" Виведення - " + Arrays.toString(a));
+        Arrays.sort(a);
+        System.out.println(" Виведення - " + Arrays.toString(a));
 
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < b.length; j++) {
-
-                if (a[i].equals(b[j])) {
-                    System.out.println(a[i] + " " + b[j]);
-                    res.add(a[i]);
-                }
-            }
-        }
-        System.out.println(res);
+        System.out.println(" Виведення - " + Arrays.toString(b));
+        Arrays.sort(b);
+        System.out.println(" Виведення - " + Arrays.toString(b));
 
     }
 }
