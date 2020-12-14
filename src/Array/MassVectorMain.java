d780910553f418302d9ec71c6e925ec9ca57940f
Yaroslav Bychkovskyi <yaroslav1208@gmail.com>
2020-12-14 18:07:25 +0200

    Сортування точок 1

--- src/Array/MassVectorMain.java
+++ src/Array/MassVectorMain.java
@@ -9,25 +9,27 @@ public class MassVectorMain {
 
     public static void main(String[] args) {
 
-        Vector v1 = new Vector(new Point(12, 56), new Point(45, 89));
+        Vector v1 = new Vector(new Point(12, 56), new Point(20, 20));
         Vector v2 = new Vector(new Point(4, 6), new Point(125, 235));
         Vector v3 = new Vector(new Point(17, 5), new Point(10, 25));
         Vector v4 = new Vector(new Point(4, 6), new Point(47, 58));
-        Vector v5 = new Vector(new Point(20, 1), new Point(36, 12));
+        Vector v5 = new Vector(new Point(20, 20), new Point(36, 13));
 
         Vector[] mas = {v1, v2, v3, v4, v5};
 
         List<Point> points = new ArrayList<>();
 
-        for (int i = 1; i < mas.length; i++) {
+        for (int i = 0; i < mas.length; i++) {
 
-            points.add(mas[i].head);
-            points.add(mas[i].tail);
-
+            if (!points.contains(mas[i].head)) {
+                points.add(mas[i].head);
+            }
+            if (!points.contains(mas[i].tail)) {
+                points.add(mas[i].tail);
+            }
         }
 
         Collections.sort(points);
-
         System.out.println(points);
 
     }
