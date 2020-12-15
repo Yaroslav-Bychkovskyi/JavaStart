67f4f79d9051cf6b0093235d1398fa5c05d29f33
Yaroslav Bychkovskyi <yaroslav1208@gmail.com>
2020-12-15 16:31:51 +0200

    цифри від 0 до 1000 які діляться на 11 без остачі на ціло

---
+++ src/Array/Figures.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Figures {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i <= 1000; i++) {
+
+            if ((i % 11) == 0)  {
+
+                System.out.println(i);
+            }
+        }
+
+    }
+}
+
+
